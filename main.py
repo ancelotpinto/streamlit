904948814952f79998060fa7f1f7fe661fa36b60
Ancelot Mark Pinto <162018+ancelotpinto@users.noreply.github.com>
2023-03-31 17:56:08 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def main():
+    st.set_page_config(
+        page_title="Hello",
+        page_icon="👋",
+    )
+
+    st.write("# Welcome to Streamlit! 👋")
+
+
+if __name__ == "__main__":
+    main()
